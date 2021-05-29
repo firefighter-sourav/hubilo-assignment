93969ad0c3f86aff858a5160aa082d9bcd1698da
Sourav Purkait <souravpurkait360@gmail.com>
2021-05-30 03:43:52 +0530

    Loggers added

--- src/app/services/httpInstance.js
+++ src/app/services/httpInstance.js
@@ -1,10 +1,45 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create()
-
+/**
+ * Axios request interceptor to put auth token and all the header/body modification
+ * can be done through this
+ */
 axiosInstance.interceptors.request.use(async (options) => {
   return options
 })
+/**
+ * Axios response interceptor to handle any kind of success/error
+ * This can be used log errors globally as this instance is being used
+ * throughout the application
+ */
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // validate get, create response here
+    return response
+  },
+  (error) => {
+    if (error.response) {
+      // some server side error
+      console.log('error in server response :- ', error.toJSON())
+      if (error.response.status === 500) {
+        console.error('Internal Server Error Occurred')
+      } else if (error.response.status >= 400 && error.response.status < 500) {
+        console.error('Something Went Wrong.')
+      }
+    } else if (error.request) {
+      // request initiated but no response from server, most probably bcz of network error
+      console.log('No Response Received :- ', error)
+    } else {
+      console.log(
+        'Error occured because of something unexpected :- ',
+        error.message
+      )
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export const get = async (url, options = {}) => {
   try {
